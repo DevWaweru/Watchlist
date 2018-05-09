7a691358ff9e451bf81f9985f0550dcd7cb7cdb2
Richard <richi@localhost.localdomain>
2018-05-09 18:00:54 +0300

    Wednesday's work

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 # from flask import render_template
-from flask import render_template
+from flask import render_template,redirect,url_for,request
 from app import app
-from .request import get_movies
+from .request import get_movies, get_movie, search_movie
 
 # Views
 @app.route('/')
@@ -16,11 +16,31 @@ def index():
     now_showing_movie = get_movies('now_playing')
     # print(popular_movies)
     title = 'Home - Welcome to the best Movie Review website online'
-    return render_template("index.html",title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie)
+    
+    search_movie = request.args.get('movie_query')
+
+    if search_movie:
+        return redirect(url_for('search',movie_name=search_movie))
+    else:
+        return render_template("index.html",title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie)
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
     '''
     view movie page function that returns the movie details page and its data
     '''
-    return render_template('movie.html',id=movie_id)
+    movie = get_movie(movie_id)
+    # title = f'{movie.title}'
+    title = '{}'.format(movie.title)
+    return render_template('movie.html', title = title, movie = movie)
+
+@app.route('/search/<movie_name>')
+def search(movie_name):
+    '''
+    View function to display the search results
+    '''
+    movie_name_list = movie_name.split(" ")
+    movie_name_format = "+".join(movie_name_list)
+    searched_movies = search_movie(movie_name_format)
+    title = f'search results for {movie_name}'
+    return render_template('search.html',movies = searched_movies)
